6bdadd6e6c492884f979df07b1eca7e365e9b2f4
Austin Calvelage <austin.calvelage@icloud.com>
2021-08-26 09:16:24 -0400

    Initial commit navigation for portfolio site is complete, along with home section.

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const navToggleBtn = document.querySelector(`.toggle-btn`);
+const navBarLinks = document.querySelector(`.navbar-links`);
+navToggleBtn.addEventListener(`click`, () => {
+    navBarLinks.classList.toggle(`active`);});
