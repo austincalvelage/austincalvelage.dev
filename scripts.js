8da32c247214b62f84506209efd0ece9b2cf1d1a
Austin Calvelage <austincalvelage@AUSTIN-DESKTOP.localdomain>
2022-01-07 13:00:05 -0500

    init commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 const navToggleBtn = document.querySelector(`.toggle-btn`);
 const navBarLinks = document.querySelector(`.navbar-links`);
+
 navToggleBtn.addEventListener(`click`, () => {
-    navBarLinks.classList.toggle(`active`);});
+  navBarLinks.classList.toggle(`active`);
+});
